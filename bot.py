ecdc81d61673a352c71e3539b1369f36a3d1740d
ApexRaptor-py <lucaskrkristo@gmail.com>
2026-06-09 16:33:19 +0200

    added twitch checker

--- bot.py
+++ bot.py
@@ -2,17 +2,22 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 import feedparser
+import requests 
 
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 
 ANNOUNCEMENT_CHANNEL_ID = 1513660892091256842
 WELCOME_CHANNEL_ID = 1513663145787523142
 ROLES_CHANNEL_ID = 1513869229806583948
 YOUTUBE_CHANNEL_ID = 1513906449066229850
+TWITCH_CHANNEL_ID = 1513907039968034886
 
 last_video_link = None
+twitch_is_live = False
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,6 +29,7 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 async def on_ready():
     print(f"{bot.user} is online!")
     check_youtube.start()
+    check_twitch.start()
 
 @bot.command()
 async def ping(ctx):
@@ -133,6 +139,59 @@ async def setuproles(ctx):
 
         await ctx.send("✅ Permanent role message created.")
 
+def get_twitch_access_token():
+    url = "https://id.twitch.tv/oauth2/token"
 
+    params = {
+        "client_id": TWITCH_CLIENT_ID,
+        "client_secret": TWITCH_CLIENT_SECRET,
+        "grant_type": "client_credentials"
+    }
+
+    response = requests.post(url, params=params)
+    data = response.json()
+
+    return data["access_token"]
+
+def is_twitch_live():
+    access_token = get_twitch_access_token()
+
+    url = "https://api.twitch.tv/helix/streams"
+
+    headers = {
+        "Client-ID": TWITCH_CLIENT_ID,
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    params = {
+        "user_login": "apexx_raptorr"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    return len(data["data"]) > 0
+
+@tasks.loop(minutes=2)
+async def check_twitch():
+    global twitch_is_live
+
+    live_now = is_twitch_live()
+
+    if live_now and not twitch_is_live:
+        twitch_is_live = True
+
+        channel = bot.get_channel(TWITCH_CHANNEL_ID)
+
+        if channel:
+            await channel.send(
+                "@everyone\n\n"
+                "🔴 **Apexx_Raptorr is LIVE on Twitch!**\n\n"
+                "Come join the stream!\n\n"
+                "🔗 https://www.twitch.tv/apexx_raptorr"
+            )
+
+    if not live_now:
+        twitch_is_live = False
 
 bot.run(TOKEN)
